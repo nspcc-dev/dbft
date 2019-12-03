f9889dac1c751c437bd767c5b73824f78bb31afc
Evgenii Stratonikov <evgeniy@nspcc.ru>
2019-12-03 12:20:58 +0300

    Add test for message cache

---
+++ helpers_test.go
@@ -0,0 +1,38 @@
+package dbft
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/nspcc-dev/dbft/payload"
+)
+
+func TestMessageCache(t *testing.T) {
+	c := newCache()
+
+	p1 := payload.NewConsensusPayload()
+	p1.SetHeight(3)
+	p1.SetType(payload.PrepareRequestType)
+	c.addMessage(p1)
+
+	p2 := payload.NewConsensusPayload()
+	p2.SetHeight(4)
+	p2.SetType(payload.ChangeViewType)
+	c.addMessage(p2)
+
+	p3 := payload.NewConsensusPayload()
+	p3.SetHeight(4)
+	p3.SetType(payload.CommitType)
+	c.addMessage(p3)
+
+	box := c.getHeight(3)
+	require.Len(t, box.chViews, 0)
+	require.Len(t, box.prepare, 1)
+	require.Len(t, box.commit, 0)
+
+	box = c.getHeight(4)
+	require.Len(t, box.chViews, 1)
+	require.Len(t, box.prepare, 0)
+	require.Len(t, box.commit, 1)
+}
