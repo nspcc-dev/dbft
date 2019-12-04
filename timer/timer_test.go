79c2a4237c261f217f7334c29f6784862eea4229
Evgenii Stratonikov <evgeniy@nspcc.ru>
2019-12-04 11:56:15 +0300

    Simplify timer tests

--- timer/timer_test.go
+++ timer/timer_test.go
@@ -12,52 +12,41 @@ func TestTimer_Reset(t *testing.T) {
 
 	tt.Reset(HV{Height: 1, View: 2}, time.Millisecond*100)
 	tt.Sleep(time.Millisecond * 200)
-	select {
-	case hv := <-tt.C():
-		require.Equal(t, hv, HV{Height: 1, View: 2})
-	default:
-		require.Fail(t, "no value in timer")
-	}
+	shouldReceive(t, tt, HV{Height: 1, View: 2}, "no value in timer")
 
 	tt.Reset(HV{Height: 1, View: 2}, time.Second)
 	tt.Reset(HV{Height: 2, View: 3}, 0)
-	select {
-	case hv := <-tt.C():
-		require.Equal(t, hv, HV{Height: 2, View: 3})
-	default:
-		require.Fail(t, "no value in timer after reset(0)")
-	}
+	shouldReceive(t, tt, HV{Height: 2, View: 3}, "no value in timer after reset(0)")
 
 	tt.Reset(HV{Height: 3, View: 1}, time.Millisecond*100)
-	select {
-	case <-tt.C():
-		require.Fail(t, "value arrived to early")
-	default:
-	}
+	shouldNotReceive(t, tt, "value arrived too early")
 
 	tt.Extend(4)
-
 	tt.Sleep(time.Millisecond * 200)
-	select {
-	case <-tt.C():
-		require.Fail(t, "value arrived to early")
-	default:
-	}
+	shouldNotReceive(t, tt, "value arrived too early after extend")
 
 	tt.Sleep(time.Millisecond * 300)
-	select {
-	case hv := <-tt.C():
-		require.Equal(t, hv, HV{Height: 3, View: 1})
-	default:
-		require.Fail(t, "no value in timer after extend")
-	}
+	shouldReceive(t, tt, HV{Height: 3, View: 1}, "no value in timer after extend")
 
 	tt.Reset(HV{1, 1}, time.Millisecond*100)
 	tt.Stop()
 	tt.Sleep(time.Millisecond * 200)
+	shouldNotReceive(t, tt, "timer was not stopped")
+}
+
+func shouldReceive(t *testing.T, tt Timer, hv HV, msg string) {
+	select {
+	case got := <-tt.C():
+		require.Equal(t, hv, got)
+	default:
+		require.Fail(t, msg)
+	}
+}
+
+func shouldNotReceive(t *testing.T, tt Timer, msg string) {
 	select {
 	case <-tt.C():
-		require.Fail(t, "timer was not cancelled")
+		require.Fail(t, msg)
 	default:
 	}
 }
