4c7e221335596040eb9ae164b0668d5564b08106
Roman Khimov <roman@nspcc.ru>
2021-11-26 18:21:06 +0300

    check: set lastBlockTime in PrepareRequest handler, fix #55
    It's the earliest point, if a view change happens the timer is reset (because
    new view comes with a new set of transactions, potentially picking up ones
    received between views).
    
    Signed-off-by: Roman Khimov <roman@nspcc.ru>

--- check.go
+++ check.go
@@ -5,6 +5,13 @@ import (
 )
 
 func (d *DBFT[H]) checkPrepare() {
+	if d.lastBlockIndex != d.BlockIndex || d.lastBlockView != d.ViewNumber {
+		// Notice that lastBlockTimestamp is left unchanged because
+		// this must be the value from the last header.
+		d.lastBlockTime = d.Timer.Now()
+		d.lastBlockIndex = d.BlockIndex
+		d.lastBlockView = d.ViewNumber
+	}
 	if !d.hasAllTransactions() {
 		d.Logger.Debug("check prepare: some transactions are missing", zap.Any("hashes", d.MissingTransactions))
 		return
@@ -57,8 +64,6 @@ func (d *DBFT[H]) checkCommit() {
 		return
 	}
 
-	d.lastBlockIndex = d.BlockIndex
-	d.lastBlockTime = d.Timer.Now()
 	d.block = d.CreateBlock()
 	hash := d.block.Hash()
 
